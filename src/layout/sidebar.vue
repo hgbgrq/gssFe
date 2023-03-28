5d3a581e578a95a2cd08fb052c7e6b2bd3520998
hgbgrq <57474913+hgbgrq@users.noreply.github.com>
2023-03-28 16:48:10 +0900

    사이드바 추가

--- src/layout/sidebar.vue
+++ src/layout/sidebar.vue
@@ -32,3 +32,37 @@ const activeMenu = (targetId: string, menu: string) => {
 }
 
 </script>
+
+<template>
+    <nav class = "sidebar">
+        <ul class="flex-1 mb-2 sidebar__wrap">
+            <li v-for="menu in adminPage.menuList" :key="menu.menuId"
+            @click="activeMenu(menu)">
+                <div class="font__subtitle">
+                    <div class="menu">
+                        {{menu.menu}}
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  position: fixed;
+  top: 50px;
+  bottom: 0;
+  left: 0;
+  z-index: 600;
+  width: 220px;
+  background-color: #484d5b;
+  text-align: center;
+}
+.font__subtitle {
+    padding: 8px 0;
+    color: white;
+    font-weight: 500;
+    cursor: default;
+}
+</style>
