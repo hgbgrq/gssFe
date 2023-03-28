3d7a3a1cba78503ad1e3f5d06e7b494f3997fbd6
hgbgrq <57474913+hgbgrq@users.noreply.github.com>
2023-03-28 16:31:06 +0900

    헤더 사이드바 추가

---
+++ src/layout/header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header class="header">
+        <span class = "logo__title">
+            hgb
+        </span>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  min-width: 1536px;
+  height: 50px;
+  padding: 10px 10px;
+  background-color: #f5f5dc;
+  box-sizing: border-box;
+}
+.logo {
+    cursor: pointer;
+    -webkit-user-select: none; // drag unset
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+.logo__title{
+  color: black;
+  cursor: default;
+  margin-left: 3%;
+  font-size: 15pt;
+  padding-bottom: 4px;
+}
+</style>
