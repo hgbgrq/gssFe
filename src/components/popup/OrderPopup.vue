89109a049b7eec4badf82e5a61b36335148a65c2
hgbgrq <57474913+hgbgrq@users.noreply.github.com>
2024-01-08 17:19:15 +0900

    [fix] 발주서 수정시 데이터 형식 변경 추가

--- src/components/popup/OrderPopup.vue
+++ src/components/popup/OrderPopup.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import dayjs from 'dayjs'
 import type { Popup } from '~/components/types/popup'
+import type { Organization } from '~/order/types'
 
 const props = defineProps<{
   orderId: string
@@ -11,7 +13,7 @@ const emit = defineEmits([
 ])
 
 interface Product {
-  productId: String
+  productId: string
   productStyleNo: string
   productItem: string
   productSize: string
@@ -34,7 +36,7 @@ const popup: Popup = reactive({
   title: '',
   show: true,
   width: '1000px',
-  button: '',
+  button: '수정',
 })
 
 const orderInfo = reactive<OrderDetail>({
@@ -46,9 +48,28 @@ const orderInfo = reactive<OrderDetail>({
   productList: [],
 })
 
+const org = ref('')
+const organizations = ref<Organization[]>([])
+const selectedOrg = ref('')
+
 const close = () => {
   emit('close')
   emit('reload')
+}
+
+const createFilter = (queryString: string) => {
+  return (organization: Organization) => {
+    return (
+      organization.orgName.toLowerCase().includes(queryString.toLowerCase())
+    )
+  }
+}
+
+const querySearch = (queryString: string, cb: any) => {
+  const results = queryString
+    ? organizations.value.filter(createFilter(queryString))
+    : organizations.value
+  cb(results)
 }
 
 const getDetailOrder = async () => {
@@ -65,10 +86,43 @@ const getDetailOrder = async () => {
     console.log(error)
   }
 }
+const getOrganizationList = async () => {
+  try {
+    const res = await request('/org', { method: 'GET' })
+    organizations.value = res.list
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+const handleSelect = (item: Organization) => {
+  selectedOrg.value = JSON.parse(
+    JSON.stringify(item.orgId),
+  )
+}
+
+const popupButton = async () => {
+  const data = {
+    ...orderInfo,
+  }
+
+  try {
+    data.orderOrderingDate = dayjs(orderInfo.orderOrderingDate).format('YYYY-MM-DD')
+    data.orderDeadLineDate = dayjs(orderInfo.orderDeadLineDate).format('YYYY-MM-DD')
+    const res = await request('/order/modify', { method: 'POST', data })
+    if (res.code === '200')
+      close()
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
 
 onMounted(async () => {
   popup.title = '발주서'
   popup.button = '수정'
+  await getOrganizationList()
   await getDetailOrder()
 })
 </script>
@@ -84,8 +138,76 @@ onMounted(async () => {
     @close="close"
   >
     <div class="popup">
-      업데이트 예정 기능입니다.
+      <div class="search-box">
+        <div class="search-form">
+          <span class="serach-label"> 회사 </span>
+          <div>
+            <el-autocomplete
+              v-model="orderInfo.orgName"
+              value-key="orgName"
+              :fetch-suggestions="querySearch"
+              clearable
+              class="inline-input w-50"
+              placeholder="회사 선택"
+              @select="handleSelect"
+            />
+          </div>
+        </div>
+        <div class="search-form">
+          <span class="serach-label"> 발주일 </span>
+          <el-date-picker v-model="orderInfo.orderOrderingDate" type="date" />
+        </div>
+        <div class="search-form">
+          <span class="serach-label"> 납기일 </span>
+          <el-date-picker v-model="orderInfo.orderDeadLineDate" type="date" />
+        </div>
+      </div>
+
+      <div>
+        <el-table :data="orderInfo.productList" style="width: 100%">
+          <el-table-column label="StyleNo" width="150">
+            <template #default="scope">
+              <el-input v-model="orderInfo.productList[scope.$index].productStyleNo" />
+            </template>
+          </el-table-column>
+          <el-table-column label="Item" width="150">
+            <template #default="scope">
+              <el-input v-model="orderInfo.productList[scope.$index].productItem" />
+            </template>
+          </el-table-column>
+          <el-table-column label="Size" width="150">
+            <template #default="scope">
+              <el-input v-model="orderInfo.productList[scope.$index].productSize" />
+            </template>
+          </el-table-column>
+          <el-table-column label="Color" width="150">
+            <template #default="scope">
+              <el-input v-model="orderInfo.productList[scope.$index].productColor" />
+            </template>
+          </el-table-column>
+          <el-table-column label="Qty" width="150">
+            <template #default="scope">
+              <el-input v-model="orderInfo.productList[scope.$index].productQty" type="number" />
+            </template>
+          </el-table-column>
+          <el-table-column label="Etc" width="150">
+            <template #default="scope">
+              <el-input v-model="orderInfo.productList[scope.$index].productEtc" />
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </div>
+    <template #footer>
+      <div class="footer">
+        <el-button type="info" @click="close">
+          취소
+        </el-button>
+        <el-button type="primary" @click="popupButton">
+          {{ popup.button }}
+        </el-button>
+      </div>
+    </template>
   </el-dialog>
 </template>
 
@@ -94,5 +216,23 @@ onMounted(async () => {
     height: 100%;
     overflow-y: auto;
     padding: 0 40px!important;
+  }
+  .search-box {
+    padding: 10px;
+    background: #ffffff;
+    border: 1px solid #c1c2c3;
+    border-radius: 10px;
+    margin-bottom: 60px;
+  }
+
+  .search-form {
+    width: 70%;
+    position: relative;
+    justify-content: start;
+    margin-bottom: 30px;
+  }
+
+  .serach-label {
+    margin-right: 5px;
   }
 </style>
